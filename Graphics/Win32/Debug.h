2927c57334bde279ac6f2353fb6f4dd7ae3f413b
takuya <takuya0330_a@yahoo.co.jp>
2024-06-01 10:13:46 +0900

    do while で if 文を囲むように修正

--- Graphics/Win32/Debug.h
+++ Graphics/Win32/Debug.h
@@ -6,31 +6,43 @@
 #include <iostream>
 
 #define ASSERT_RETURN(expr, ...) \
-	if (!(expr))                 \
+	do                           \
 	{                            \
-		assert(expr);            \
-		return __VA_ARGS__;      \
-	}
+		if (!(expr))             \
+		{                        \
+			assert(expr);        \
+			return __VA_ARGS__;  \
+		}                        \
+	} while (false)
 
-#define ASSERT_IF_FAILED(hr)                     \
-	if (FAILED(hr))                              \
-	{                                            \
-		printf("ERROR: HRESULT = 0x%08X\n", hr); \
-		assert(SUCCEEDED(hr));                   \
-	}
+#define ASSERT_IF_FAILED(hr)                         \
+	do                                               \
+	{                                                \
+		if (FAILED(hr))                              \
+		{                                            \
+			printf("ERROR: HRESULT = 0x%08X\n", hr); \
+			assert(SUCCEEDED(hr));                   \
+		}                                            \
+	} while (false)
 
-#define RETURN_HRESULT_IF_FAILED(hr)             \
-	if (FAILED(hr))                              \
-	{                                            \
-		printf("ERROR: HRESULT = 0x%08X\n", hr); \
-		return hr;                               \
-	}
+#define RETURN_HRESULT_IF_FAILED(hr)                 \
+	do                                               \
+	{                                                \
+		if (FAILED(hr))                              \
+		{                                            \
+			printf("ERROR: HRESULT = 0x%08X\n", hr); \
+			return hr;                               \
+		}                                            \
+	} while (false)
 
-#define RETURN_FALSE_IF_FAILED(hr)               \
-	if (FAILED(hr))                              \
-	{                                            \
-		printf("ERROR: HRESULT = 0x%08X\n", hr); \
-		return false;                            \
-	}
+#define RETURN_FALSE_IF_FAILED(hr)                   \
+	do                                               \
+	{                                                \
+		if (FAILED(hr))                              \
+		{                                            \
+			printf("ERROR: HRESULT = 0x%08X\n", hr); \
+			return false;                            \
+		}                                            \
+	} while (false)
 
 #define ENABLE_IMGUI_DEMO_WINDOW 0
